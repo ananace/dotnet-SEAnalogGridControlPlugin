89dd5ebe1b5e5672d348f095a2e22a0923360b9c
Alexander Olofsson <me@ananace.dev>
2025-09-09 10:47:51 +0200

    Add separate log file

--- AnalogGridControl/Util/MyPluginLog.cs
+++ AnalogGridControl/Util/MyPluginLog.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using VRage.FileSystem;
 using VRage.Utils;
 
 namespace AnanaceDev.AnalogGridControl.Util
@@ -6,6 +8,7 @@ namespace AnanaceDev.AnalogGridControl.Util
 
   public enum MyPluginLogLevel
   {
+    ERROR,
     INFO,
     WARNING,
     DEBUG
@@ -13,6 +16,15 @@ namespace AnanaceDev.AnalogGridControl.Util
 
   public static class MyPluginLog
   {
+    private const string FileName = "AnalogGridControlPlugin.log";
+    private static string FilePath => Path.Combine(MyFileSystem.UserDataPath, FileName);
+
+    private static Lazy<string> _LogFile = new Lazy<string>(() => {
+      System.IO.File.WriteAllText(FilePath, $"{DateTime.Now.ToString("s")} Logging started.\n");
+      return FilePath;
+    });
+    private static string LogFile => _LogFile.Value;
+
     public static void Info(string Message)
     {
       WriteMessage(MyPluginLogLevel.INFO, Message);
@@ -21,6 +33,11 @@ namespace AnanaceDev.AnalogGridControl.Util
     public static void Warning(string Message)
     {
       WriteMessage(MyPluginLogLevel.WARNING, Message);
+    }
+
+    public static void Error(string Message)
+    {
+      WriteMessage(MyPluginLogLevel.ERROR, Message);
     }
 
     public static void Debug(string Message)
@@ -33,6 +50,7 @@ namespace AnanaceDev.AnalogGridControl.Util
 
     private static void WriteMessage(MyPluginLogLevel Level, string Message)
     {
+      File.AppendAllText(LogFile, $"{DateTime.Now.ToString("s")} {Level.ToString().PadLeft(7)} -> {Message}\n");
       MyLog.Default.WriteLine($"[AnalogGridControl|{Level}] {Message}");
     }
   }
